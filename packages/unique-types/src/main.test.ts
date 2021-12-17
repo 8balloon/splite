06bbfc9785b758647281ae6e6a8c2434ef46ef69
Ethan Gregory Clark <ethangclark@gmail.com>
2021-12-16 20:42:43 -0700

    rename to "Autographed Type"

--- packages/unique-types/src/main.test.ts
+++ packages/unique-types/src/main.test.ts
@@ -1,30 +1,30 @@
-import { VerifiedType, TypeVerificationError, VerifiedTypeAsync } from "./main";
+import { AutographedType, AutographError, AutographedTypeAsync } from "./main";
 
 declare const IntegerSignature: unique symbol;
-const Integer = VerifiedType<number, typeof IntegerSignature>(
+const Integer = AutographedType<number, typeof IntegerSignature>(
   (n) => parseInt(n.toString()) === n
 );
 
 describe("verified type", () => {
   it(`can be created`, () => {
-    expect(Integer.create(2)).toEqual(2);
+    expect(Integer.sign(2)).toEqual(2);
   });
   it(`rejects value that fails verification`, () => {
-    expect(() => Integer.create(1.5)).toThrow(TypeVerificationError);
+    expect(() => Integer.sign(1.5)).toThrow(AutographError);
   });
 });
 
 declare const OddNumberSignature: unique symbol;
-const OddNumber = VerifiedTypeAsync<number, typeof OddNumberSignature>((n) =>
+const OddNumber = AutographedTypeAsync<number, typeof OddNumberSignature>((n) =>
   Promise.resolve(n % 2 === 1)
 );
 
 describe("async verified type", () => {
   it(`can be created`, async () => {
-    const result = await OddNumber.create(3);
+    const result = await OddNumber.sign(3);
     expect(result).toEqual(3);
   });
   it(`rejects value that fails verification`, async () => {
-    await expect(OddNumber.create(2)).rejects.toThrow(TypeVerificationError);
+    await expect(OddNumber.sign(2)).rejects.toThrow(AutographError);
   });
 });
--- packages/unique-types/src/main.ts
+++ packages/unique-types/src/main.ts
@@ -1,40 +1,33 @@
-export type Signed<T, UuniqueSymbolSignature extends symbol> = T & {
-  signature: UuniqueSymbolSignature;
+export type autographed<T, UuniqueSymbolAutograph extends symbol> = T & {
+  autograph: UuniqueSymbolAutograph;
 };
 
-export class TypeVerificationError extends TypeError {}
+export class AutographError extends TypeError {}
 
-export function VerifiedType<T, UuniqueSymbolSignature extends symbol>(
-  verifies: (candidate: T) => boolean
+export function AutographedType<T, UuniqueSymbolAutograph extends symbol>(
+  accepts: (candidate: T) => boolean
 ) {
   return {
-    create(base: T) {
-      if (verifies(base) === false) {
-        throw new TypeVerificationError();
+    sign(base: T) {
+      if (accepts(base) === false) {
+        throw new AutographError();
       }
-      return base as Signed<T, UuniqueSymbolSignature>;
+      return base as autographed<T, UuniqueSymbolAutograph>;
     },
-    verifies,
+    accepts,
   };
 }
 
-export function VerifiedTypeAsync<T, UuniqueSymbolSignature extends symbol>(
-  verifies: (candidate: T) => Promise<boolean>
+export function AutographedTypeAsync<T, UuniqueSymbolAutograph extends symbol>(
+  accepts: (candidate: T) => Promise<boolean>
 ) {
   return {
-    async create(base: T) {
-      if ((await verifies(base)) === false) {
-        throw new TypeVerificationError();
+    async sign(base: T) {
+      if ((await accepts(base)) === false) {
+        throw new AutographError();
       }
-      return base as Signed<T, UuniqueSymbolSignature>;
+      return base as autographed<T, UuniqueSymbolAutograph>;
     },
-    verifies,
+    accepts,
   };
 }
-
-// TODO: async support
-
-/*
-^^ SIGNED types (verified types? verified = signed + checked) (include & { signature: uniqueSignatureSymbol })
-BRANDED types: https://medium.com/@KevinBGreene/surviving-the-typescript-ecosystem-branding-and-type-tagging-6cf6e516523d / https://github.com/pelotom/runtypes
-*/
