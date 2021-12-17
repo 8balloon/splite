9782b6839cc62ac7314a217ec0cb98152b6e11ab
Ethan Gregory Clark <ethangclark@gmail.com>
2021-12-16 20:46:03 -0700

    reassignment test

--- packages/unique-types/src/main.test.ts
+++ packages/unique-types/src/main.test.ts
@@ -9,6 +9,12 @@ describe("verified type", () => {
   it(`can be created`, () => {
     expect(Integer.sign(2)).toEqual(2);
   });
+  it(`can be reassigned`, () => {
+    let result = Integer.sign(2);
+    result = Integer.sign(3);
+    // result = 4 // typescript error
+    expect(result).toEqual(3);
+  });
   it(`rejects value that fails verification`, () => {
     expect(() => Integer.sign(1.5)).toThrow(AutographError);
   });
@@ -23,6 +29,12 @@ describe("async verified type", () => {
   it(`can be created`, async () => {
     const result = await OddNumber.sign(3);
     expect(result).toEqual(3);
+  });
+  it(`can be reassigned`, async () => {
+    let result = await OddNumber.sign(3);
+    result = await OddNumber.sign(5);
+    // result = 44 // typescript error
+    expect(result).toEqual(5);
   });
   it(`rejects value that fails verification`, async () => {
     await expect(OddNumber.sign(2)).rejects.toThrow(AutographError);
